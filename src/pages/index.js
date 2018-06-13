75e08e839007d567f62ca18c5fa26cb4b8a40c2d
Jani Anttonen <jantto@utu.fi>
2018-06-13 21:44:36 +0300

    font is still not working, but is alright lul

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react'
 import Link from 'gatsby-link'
 
+import './intro.css'
+
 // TODO: https://www.goodreads.com/api
 
 const IndexPage = ({ data }) => {
   return (
     <div>
-      <style>{`
-      #intro {
-        font-family: "Aadhunik";
-        font-size: 13px;
-        color: #c0bdf0;
-      }
-    `}</style>
       <div id="intro">Jani Anttonen</div>
     </div>
   )
