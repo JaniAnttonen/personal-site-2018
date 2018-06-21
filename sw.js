ea9a58bdfd366feaa90bc51b63707883eb940992
Jani Anttonen <jantto@utu.fi>
2018-06-21 12:32:31 +0300

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-3498a0a161d14de64a90.js","08bf4ef47524c6015ff58bf23917046f"],["/app-b1eec7af6df5ce7031e7.js","322da7a4115282276c075b379d536f15"],["/app-cd3d068d9c4ce7ce13cb.js","561da33f432be026985f7a49bedc837c"],["/commons-afb5782224ac01f1fa03.js","fc13fae2343302a46ab6dcb66be32ab2"],["/index.html","ec2a849c9d25f9d0d0d9792eec097cee"],["/manifest.webmanifest","4a211109c5c0175f4d398ffdc080a383"],["/offline-plugin-app-shell-fallback/index.html","13bafd9f05a51ff69d48b33df90ba0a2"]];
+var precacheConfig = [["/app-3498a0a161d14de64a90.js","6c10ce6f61ce454127fb0a4bf7bdc9e3"],["/app-b1eec7af6df5ce7031e7.js","322da7a4115282276c075b379d536f15"],["/app-cd3d068d9c4ce7ce13cb.js","561da33f432be026985f7a49bedc837c"],["/commons-afb5782224ac01f1fa03.js","fc13fae2343302a46ab6dcb66be32ab2"],["/commons-e046370cf6b52a293e1e.js","7d62a73e9615bbeefb4a14104a44b1c4"],["/index.html","69a4a9cca17516fee249afa799e05968"],["/manifest.webmanifest","4a211109c5c0175f4d398ffdc080a383"],["/offline-plugin-app-shell-fallback/index.html","d131cef032d4195eaa1f5f6f9252b6e4"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
