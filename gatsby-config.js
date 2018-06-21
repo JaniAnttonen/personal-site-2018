5a5d9ec74a6c1b2a27566988b5c31c979a85edb4
Jani Anttonen <jantto@utu.fi>
2018-06-21 03:18:04 +0300

    add manifest configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,18 @@ module.exports = {
       },
     },
     'gatsby-transformer-remark',
-    'gatsby-plugin-manifest',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: "Jani Anttonen",
+        short_name: "jantto",
+        start_url: "/",
+        background_color: "#09051a",
+        theme_color: "#c0bdf0",
+        display: "minimal-ui",
+        icon: "src/assets/images/favicon.png"
+      },
+    },
     'gatsby-plugin-offline'
   ]
 }
