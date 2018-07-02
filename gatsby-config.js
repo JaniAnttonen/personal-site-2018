b65bd78380855753b2bc64ae729ad2c7b78aa1f7
Jani Anttonen <jantto@utu.fi>
2018-07-02 19:36:42 +0300

    correct config for prismjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,22 @@ module.exports = {
         name: 'blog-posts',
       },
     },
-    'gatsby-transformer-remark',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
