6096dbbb6617bcf63ad558d1c362041f218cb8a0
Jani Anttonen <jantto@utu.fi>
2019-01-15 01:38:00 +0200

    some very dirty, nasty and naughty fixes due to goodreads API's very good status

--- src/pages/books.js
+++ src/pages/books.js
@@ -26,14 +26,14 @@ class GoodReads extends Component {
     const HAVE_READ_WIDGET_ID = '1529534706'
 
     const readingRes = await ky(
-      `https://www.goodreads.com/review/grid_widget/73247619.Jani's%20currently-reading%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=currently-reading&sort=date_added&widget_id=${NOW_READING_WIDGET_ID}`
+      `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20currently-reading%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=currently-reading&sort=date_added&widget_id=${NOW_READING_WIDGET_ID}`
     ).text()
     const readingElem = {
       __html: Parser.parse(readingRes).body[0].declarations[0].init.value,
     }
 
     const readRes = await ky(
-      `https://www.goodreads.com/review/grid_widget/73247619.Jani's%20read%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=read&sort=date_added&widget_id=${HAVE_READ_WIDGET_ID}`
+      `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20read%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=read&sort=date_added&widget_id=${HAVE_READ_WIDGET_ID}`
     ).text()
     const readElem = {
       __html: Parser.parse(readRes).body[0].declarations[0].init.value,
