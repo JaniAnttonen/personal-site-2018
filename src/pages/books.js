a07b517650b1163941e27072c52aa4cdaa1a7f4f
Jani Anttonen <jantto@utu.fi>
2019-01-15 23:46:16 +0200

    cache book fetches

--- src/pages/books.js
+++ src/pages/books.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { setConfig } from 'react-hot-loader'
 import ky from 'ky'
 import { Parser } from 'acorn'
+import { addDays, isAfter } from 'date-fns'
 
 import Layout from '../components/layout'
 import './books.css'
@@ -21,28 +22,65 @@ class GoodReads extends Component {
     await this.fetchGoodReadsAPI()
   }
 
+  updateCache(janiBooksCacheObject) {
+    if (window.localStorage) {
+      janiBooksCacheObject.timestamp = new Date(Date.now())
+      window.localStorage.setItem(
+        'janianttonen.com:cache',
+        JSON.stringify(janiBooksCacheObject)
+      )
+    }
+  }
+
+  getCache() {
+    const returnable = { reading: false, read: false }
+    if (window.localStorage) {
+      let cache = window.localStorage.getItem('janianttonen.com:cache')
+      if (cache) {
+        cache = JSON.parse(cache)
+        const lastCachePlusTwoDays = addDays(new Date(cache.timestamp), 2)
+        const now = new Date(Date.now())
+        if (isAfter(lastCachePlusTwoDays, now)) {
+          returnable.reading = cache.reading
+          returnable.read = cache.read
+        }
+      }
+    }
+    return returnable
+  }
+
   async fetchGoodReadsAPI() {
-    const NOW_READING_WIDGET_ID = '1529572489'
-    const HAVE_READ_WIDGET_ID = '1529534706'
+    const cache = this.getCache()
 
-    const readingRes = await ky(
-      `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20currently-reading%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=currently-reading&sort=date_added&widget_id=${NOW_READING_WIDGET_ID}`
-    ).text()
-    const readingElem = {
-      __html: Parser.parse(readingRes).body[0].declarations[0].init.value,
+    if (cache.read && cache.reading) {
+      this.setState({
+        nowReading: { __html: cache.reading },
+        haveRead: { __html: cache.read },
+      })
+    } else {
+      const NOW_READING_WIDGET_ID = '1529572489'
+      const HAVE_READ_WIDGET_ID = '1529534706'
+
+      const readingRes = await ky(
+        `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20currently-reading%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=6&order=a&shelf=currently-reading&sort=date_added&widget_id=${NOW_READING_WIDGET_ID}`
+      ).text()
+      const readingElem = {
+        __html: Parser.parse(readingRes).body[0].declarations[0].init.value,
+      }
+
+      const readRes = await ky(
+        `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20read%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=9&order=a&shelf=read&sort=date_added&widget_id=${HAVE_READ_WIDGET_ID}`
+      ).text()
+      const readElem = {
+        __html: Parser.parse(readRes).body[0].declarations[0].init.value,
+      }
+
+      this.updateCache({ reading: readingElem.__html, read: readElem.__html })
+      this.setState({
+        nowReading: readingElem,
+        haveRead: readElem,
+      })
     }
-
-    const readRes = await ky(
-      `https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/grid_widget/73247619.Jani's%20read%20book%20montage?cover_size=medium&hide_link=true&hide_title=true&num_books=20&order=a&shelf=read&sort=date_added&widget_id=${HAVE_READ_WIDGET_ID}`
-    ).text()
-    const readElem = {
-      __html: Parser.parse(readRes).body[0].declarations[0].init.value,
-    }
-
-    this.setState({
-      nowReading: readingElem,
-      haveRead: readElem,
-    })
   }
 
   render() {
@@ -50,16 +88,28 @@ class GoodReads extends Component {
     return (
       <React.Fragment>
         <p>Books that I'm currently reading:</p>
-        {nowReading && (
+        {nowReading ? (
           <section
             className="now-reading"
             dangerouslySetInnerHTML={nowReading}
           />
+        ) : (
+          <section className="now-reading">
+            <div class="gr_grid_book_container" />
+            <div class="gr_grid_book_container" />
+            <div class="gr_grid_book_container" />
+          </section>
         )}
 
         <p>Books that I've read:</p>
-        {haveRead && (
+        {haveRead ? (
           <section className="read" dangerouslySetInnerHTML={haveRead} />
+        ) : (
+          <section className="read">
+            <div class="gr_grid_book_container" />
+            <div class="gr_grid_book_container" />
+            <div class="gr_grid_book_container" />
+          </section>
         )}
       </React.Fragment>
     )
