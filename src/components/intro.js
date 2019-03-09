eb04a04d8b19baa20dc151f5ff5695a7b15e3f3b
Jani Anttonen <jantto@utu.fi>
2019-03-09 15:31:38 +0200

    update intro text

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,14 +2,20 @@ import React from 'react'
 import './intro.css'
 import './nav.css'
 
-const Intro = ({ location }) => location &&
-location.pathname === '/' && (
-  <header className="intro animated fadeIn">
-        <span className="animated fadeIn"><strong>Jani Anttonen</strong> – A creative mind with a broad view of
-    cutting edge technologies. Expert in designing and building web applications
-    with ~10 years of experience. Has dove in the deep end using deep learning
-    and statistics in the last few years.</span>
-  </header>
-)
+const Intro = ({ location }) =>
+  location &&
+  location.pathname === '/' && (
+    <header className="intro animated fadeIn">
+      <span className="animated fadeIn">
+        <strong>Jani Anttonen</strong> – A creative mind with a broad view of
+        cutting edge technologies. Expert in designing and building web
+        applications with over 10 years of experience. Currently working on
+        distributed systems, and on{' '}
+        <a href="https://marshallai.com" target="_blank">
+          physical security at MarshallAI.
+        </a>{' '}
+      </span>
+    </header>
+  )
 
 export default Intro
